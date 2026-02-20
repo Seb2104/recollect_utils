99349e72676fcc703887273bc105e9d37b540bb8
LAPTOP/sebas <sebastiantomlinsonnel@gmail.com>
2026-02-19 19:54:45 -1000

    #16 - Added suggestion_menu

--- lib/modules/menus/menus.dart
+++ lib/modules/menus/menus.dart
@@ -1,9 +1,10 @@
 library menus;
 
-import 'package:recollect_utils/recollect_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:recollect_utils/recollect_utils.dart';
 
 part 'src/common/menu_item.dart';
 part 'src/filtered_menu.dart';
 part 'src/simple_menu.dart';
+part 'src/suggestion_menu.dart';
